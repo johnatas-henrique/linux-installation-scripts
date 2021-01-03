40416aa7eb019fc985a96ace1afe35a804abbe4f
Johnatas Henrique <johnatas.henrique@gmail.com>
2021-01-02 22:21:55 -0300

    Instala NVM e node atualizado

---
+++ nvm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt install -y curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install node
+nvm install 14
+nvm alias default 14
+
+echo Feche o terminal e abra novamente ANTES de utilizar
+
