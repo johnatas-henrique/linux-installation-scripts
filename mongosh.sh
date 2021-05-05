3f611dcf2198980842badc5f6909ac85741bb23a
Johnatas Henrique <johnatas.henrique@gmail.com>
2021-05-05 13:17:03 -0300

    Adding mongoSH

---
+++ mongosh.sh
@@ -0,0 +1,6 @@
+sudo apt install -y gnupg
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
+sudo apt install -y mongosh
