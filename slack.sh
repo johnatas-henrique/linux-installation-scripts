8ea3f1d88c09446e9fcd8e2c523a8186c4ff4894
Johnatas Henrique <johnatas.henrique@gmail.com>
2022-10-09 02:40:12 -0300

    update: slack link

--- slack.sh
+++ slack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.12.0-amd64.deb
+wget https://downloads.slack-edge.com/releases/linux/4.28.182/prod/x64/slack-desktop-4.28.182-amd64.deb
 
 sudo apt install -y ./slack-desktop-4.12.0-amd64.deb
