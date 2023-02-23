b651ee20b3d18bc19d28d7172353053a0e0e1635
Johnatas Henrique <johnatas.henrique@gmail.com>
2023-02-23 10:41:19 -0300

    feat: add MySQL Workbench install script

---
+++ workbench.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://downloads.mysql.com/archives/get/p/8/file/mysql-workbench-community_8.0.29-1ubuntu20.04_amd64.deb
+
+sudo apt install -y ./mysql-workbench-community_8.0.29-1ubuntu20.04_amd64.deb
