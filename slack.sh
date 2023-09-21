fbb7bfb8b58da196368a3fb86414e573ef35adfd
Paulo Ricardo Zambelli Taveira <64559851+pauloricardoz@users.noreply.github.com>
2023-09-21 10:44:56 -0300

    Update slack version

--- slack.sh
+++ slack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://downloads.slack-edge.com/releases/linux/4.28.182/prod/x64/slack-desktop-4.28.182-amd64.deb
+wget https://downloads.slack-edge.com/releases/linux/4.34.120/prod/x64/slack-desktop-4.34.120-amd64.deb
 
-sudo apt install -y ./slack-desktop-4.12.0-amd64.deb
+sudo apt install -y ./slack-desktop-4.34.120-amd64.deb
